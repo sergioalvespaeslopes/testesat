f5468b34de0fece7f802be88ede06f9dd581b89a
sergioalvespaeslopes <71602748+sergioalvespaeslopes@users.noreply.github.com>
2023-12-13 23:26:15 -0300

    Update index.blade.php

--- resources/views/teste/index.blade.php
+++ resources/views/teste/index.blade.php
@@ -10,7 +10,7 @@
 </head>
 <body class="container mt-5">
 
-    <h1 class="mb-4">Lista de Registros</h1>
+    <h1 class="mb-4">Lista</h1>
 
     @if(session('success'))
         <div class="alert alert-success">{{ session('success') }}</div>
